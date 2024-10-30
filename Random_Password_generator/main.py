0cf0adf23ba497ff701ab63f81219463d1a90d44
Dashuy <dariusmalekshahian@gmail.com>
2024-10-30 12:20:23 +0200

    Improved password generator

--- Random_Password_generator/main.py
+++ Random_Password_generator/main.py
@@ -1,6 +1,30 @@
-import random
+import secrets
 import string
 
 def random_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice
+    if length < 8:
+        raise ValueError("Password length should be at least 8 characters.")
+
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    punctuation = string.punctuation
+
+    password = [
+        secrets.choice(lowercase),
+        secrets.choice(uppercase),
+        secrets.choice(digits),
+        secrets.choice(punctuation)
+    ]
+
+    all_characters = lowercase + uppercase + digits + punctuation
+    password += [secrets.choice(all_characters) for _ in range(length - 4)]
+
+    secrets.SystemRandom().shuffle(password)
+
+    return ''.join(password)
+
+try:
+    print(random_password(12))  
+except ValueError as e:
+    print(e)
